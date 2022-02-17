8234e9eb69f9f4026b862bc60c42087c3c752e5e
Reinexxx <carey_caroline09@yahoo.co.jp>
2022-02-17 15:42:05 +0900

    Initial Commit

---
+++ HeartBeatOsc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeartBeatOsc
+//
+//  Created by Reinexxx on 2022/02/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeartBeatOsc/HeartBeatOscApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartBeatOscApp.swift
+//  HeartBeatOsc
+//
+//  Created by Reinexxx on 2022/02/17.
+//
+
+import SwiftUI
+
+@main
+struct HeartBeatOscApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
